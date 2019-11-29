6a3e37a6d5926ed368dc9449c5ef7ca5a487a949
Subrata Sarker <subrata_sarker@yahoo.com>
2019-11-30 00:32:32 +0900

    Accept decimal as input number

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -7,18 +7,18 @@
 */
 
 function ConvertHandler() {
-  this.regx = /^([\d]+(?:\.?[\d]+)?(?:\/[\d]+(?:\.?[\d]+)?)?)([a-zA-Z]+)?$/;
+  this.regX = /^([\d]+(?:\.?[\d]+)?(?:\/[\d]+(?:\.?[\d]+)?)?)([a-zA-Z]+)?$/;
 
   this.getNum = function (input) {
 
-    var match = input.match(this.regx);
+    var match = input.match(this.regX);
     if (match) {
       return eval(match[1]);
     }
   };
 
   this.getUnit = function (input) {
-    var match = input.match(this.regx);
+    var match = input.match(this.regX);
     if (match) {
       return match[2];
     }
