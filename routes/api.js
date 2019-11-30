632634c8e96c3d197dee67b6f767c215de97055e
Subrata Sarker <subrata_sarker@yahoo.com>
2019-11-30 17:03:23 +0900

    Update controller for /api/convert

--- routes/api.js
+++ routes/api.js
@@ -23,8 +23,14 @@ module.exports = function (app) {
       var returnNum = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
-      //res.json
+      var result = {
+        initNum: initNum,
+        initUnit: initUnit,
+        returnNum: returnNum,
+        returnUnit: returnUnit,
+        string: toString
+      };
+      res.json(result);
     });
     
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -26,7 +26,7 @@ suite('Functional Tests', function() {
         .end(function(err, res){
           assert.equal(res.status, 200);
           assert.equal(res.body.initNum, 10);
-          assert.equal(res.body.initUnit, 'L');
+          assert.equal(res.body.initUnit, 'l');
           assert.approximately(res.body.returnNum, 2.64172, 0.1);
           assert.equal(res.body.returnUnit, 'gal');
           done();
@@ -34,8 +34,15 @@ suite('Functional Tests', function() {
       });
       
       test('Convert 32g (invalid input unit)', function(done) {
-        
-        //done();
+        chai.request(server)
+        .get('/api/convert')
+        .query({input: '32g'})
+        .end(function(err, res){
+          assert.equal(res.status, 505);
+          assert.equal(res.body.initNum, 32);
+          assert.isUndefined(res.body.initUnit);
+          done();
+        });
       });
       
       test('Convert 3/7.2/4kg (invalid number)', function(done) {
